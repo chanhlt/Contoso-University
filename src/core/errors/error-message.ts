85f0fe5587e490d4dabe3a3978eb36f01445c42b
Chánh Lê <chanhlt190290.1@gmail.com>
2023-10-19 01:06:36 +0700

    refactor error message

--- src/core/errors/error-message.ts
+++ src/core/errors/error-message.ts
@@ -1,14 +1,13 @@
 import { ERROR } from './error-code';
 
 export const ERROR_MESSAGE: Record<ERROR, string> = {
-  [ERROR.STUDENT_ID_MUST_BE_A_NUMBER]: 'Student ID must be a number',
   [ERROR.STUDENT_FIRST_NAME_IS_REQUIRED]: 'Student First Name is required',
-  [ERROR.STUDENT_LAST_NAME_IS_REQUIRED]: '',
-  [ERROR.STUDENT_ENROLLMENT_DATE_IS_REQUIRED]: '',
-  [ERROR.STUDENT_DOES_NOT_EXIST]: '',
-  [ERROR.COURSE_NAME_IS_REQUIRED]: 'Course name is required',
-  [ERROR.COURSE_START_DATE_IS_REQUIRED]: 'Course start date is required',
-  [ERROR.COURSE_NOT_FOUND]: 'Course not found',
+  [ERROR.STUDENT_LAST_NAME_IS_REQUIRED]: 'Student Last Name is required',
+  [ERROR.STUDENT_ENROLLMENT_DATE_IS_REQUIRED]: 'Student Enrollment Date is required',
+  [ERROR.STUDENT_NOT_FOUND]: 'Student not found',
+  [ERROR.COURSE_NAME_IS_REQUIRED]: 'Course Name is required',
+  [ERROR.COURSE_START_DATE_IS_REQUIRED]: 'Course Start Date is required',
   [ERROR.COURSE_UPDATE_PAYLOAD_EMPTY]: 'Course update payload is empty',
+  [ERROR.COURSE_NOT_FOUND]: 'Course not found',
   [ERROR.RESOURCE_NOT_FOUND]: 'Resource not found'
 };
