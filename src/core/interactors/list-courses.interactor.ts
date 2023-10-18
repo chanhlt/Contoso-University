228c10292e64668f569a436d105427d932eac7d3
Chánh Lê <chanhlt190290.1@gmail.com>
2023-10-18 15:17:19 +0700

    add unit test for listing all courses use case

--- src/core/interactors/list-courses.interactor.ts
+++ src/core/interactors/list-courses.interactor.ts
@@ -7,13 +7,22 @@ export class ListCoursesInteractor {
     this.courseRepository = courseRepository;
   }
 
-  public async execute(
-    page: number = 1,
-    limit: number = 10
-  ): Promise<{ count: number; rows: Omit<Course, 'validate'>[] }> {
+  public async execute(page = 1, limit = 10): Promise<{ count: number; rows: Omit<Course, 'validate'>[] }> {
+    page = this.toPositiveInt(page, 1);
+    limit = this.toPositiveInt(limit, 10);
     const offset = (page - 1) * limit;
     const count = await this.courseRepository.count();
     const rows = await this.courseRepository.list(offset, limit);
     return { count, rows };
+  }
+
+  private toPositiveInt(input: number, def: number) {
+    if (!input || isNaN(input) || input <= 0) {
+      return def;
+    }
+    if (typeof input == 'string') {
+      return parseInt(input);
+    }
+    return input;
   }
 }
--- tests/core/interactors/list-courses.interactor.test.ts
+++ tests/core/interactors/list-courses.interactor.test.ts
@@ -49,4 +49,72 @@ describe('ListCoursesInteractor', () => {
     ]);
     expect(courseRepository.list).toBeCalledWith(0, 10);
   });
+
+  test('List courses with invalid page => should be OK', async () => {
+    const courseRepository = {} as ICourseRepository;
+    const name = 'Name 1';
+    const startDate = new Date();
+    const name2 = 'Name 2';
+    const startDate2 = new Date();
+    courseRepository.count = jest.fn(() => Promise.resolve(2));
+    courseRepository.list = jest.fn(() =>
+      Promise.resolve([
+        { id: 1, name, startDate },
+        { id: 2, name: name2, startDate: startDate2 }
+      ])
+    );
+    const listCoursesInteractor = new ListCoursesInteractor(courseRepository);
+    const { count, rows } = await listCoursesInteractor.execute(-1);
+    expect(count).toBe(2);
+    expect(rows).toEqual([
+      { id: 1, name, startDate },
+      { id: 2, name: name2, startDate: startDate2 }
+    ]);
+    expect(courseRepository.list).toBeCalledWith(0, 10);
+  });
+
+  test('List courses with invalid limit => should be OK', async () => {
+    const courseRepository = {} as ICourseRepository;
+    const name = 'Name 1';
+    const startDate = new Date();
+    const name2 = 'Name 2';
+    const startDate2 = new Date();
+    courseRepository.count = jest.fn(() => Promise.resolve(2));
+    courseRepository.list = jest.fn(() =>
+      Promise.resolve([
+        { id: 1, name, startDate },
+        { id: 2, name: name2, startDate: startDate2 }
+      ])
+    );
+    const listCoursesInteractor = new ListCoursesInteractor(courseRepository);
+    const { count, rows } = await listCoursesInteractor.execute(-1, 'abc' as unknown as number);
+    expect(count).toBe(2);
+    expect(rows).toEqual([
+      { id: 1, name, startDate },
+      { id: 2, name: name2, startDate: startDate2 }
+    ]);
+    expect(courseRepository.list).toBeCalledWith(0, 10);
+  });
+
+  test('List courses with over exceeded page => should be OK', async () => {
+    const courseRepository = {} as ICourseRepository;
+    courseRepository.count = jest.fn(() => Promise.resolve(2));
+    courseRepository.list = jest.fn(() => Promise.resolve([]));
+    const listCoursesInteractor = new ListCoursesInteractor(courseRepository);
+    const { count, rows } = await listCoursesInteractor.execute(2, 3);
+    expect(count).toBe(2);
+    expect(rows).toEqual([]);
+    expect(courseRepository.list).toBeCalledWith(3, 3);
+  });
+
+  test('List courses with page in string => should be OK', async () => {
+    const courseRepository = {} as ICourseRepository;
+    courseRepository.count = jest.fn(() => Promise.resolve(2));
+    courseRepository.list = jest.fn(() => Promise.resolve([]));
+    const listCoursesInteractor = new ListCoursesInteractor(courseRepository);
+    const { count, rows } = await listCoursesInteractor.execute('2' as unknown as number, 3);
+    expect(count).toBe(2);
+    expect(rows).toEqual([]);
+    expect(courseRepository.list).toBeCalledWith(3, 3);
+  });
 });
