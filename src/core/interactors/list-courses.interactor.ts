5199aa66f1de28c36cf5f8b59714d54b1da77e05
Chánh Lê <chanhlt190290.1@gmail.com>
2023-10-18 15:26:44 +0700

    refactor use case of listing all courses

--- src/core/interactors/list-courses.interactor.ts
+++ src/core/interactors/list-courses.interactor.ts
@@ -1,13 +1,15 @@
 import { ICourseRepository } from '../repositories/course.repository';
 import { Course } from '../entities/course.entity';
 
+type CourseList = { count: number; rows: Omit<Course, 'validate'>[] };
+
 export class ListCoursesInteractor {
   private readonly courseRepository: ICourseRepository;
   constructor(courseRepository: ICourseRepository) {
     this.courseRepository = courseRepository;
   }
 
-  public async execute(page = 1, limit = 10): Promise<{ count: number; rows: Omit<Course, 'validate'>[] }> {
+  public async execute(page = 1, limit = 10): Promise<CourseList> {
     page = this.toPositiveInt(page, 1);
     limit = this.toPositiveInt(limit, 10);
     const offset = (page - 1) * limit;
